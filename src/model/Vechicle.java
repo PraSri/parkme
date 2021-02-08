d70b72a360c5f9ee124fb52ea54e747043ef28ba
PraSri <prakharsri94@yahoo.com>
2021-02-09 01:35:58 +0530

    1st commit draft

---
+++ src/model/Vechicle.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface Vechicle {
+// car bike etc
+}
---
+++ src/parkinglot/Main.java
@@ -0,0 +1,9 @@
+package parkinglot;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO");
+	}
+
+}
